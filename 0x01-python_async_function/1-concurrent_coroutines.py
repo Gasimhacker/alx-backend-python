ccc6734bbb98d3e7cb210600e7d75de781b16d18
Mohamed Ahmed <ma2286572@gmail.com>
2024-07-10 00:59:33 +0000

    Call a coroutine n times

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -8,5 +8,5 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """Call an async function n times"""
     res = await asyncio.gather(
-            *tuple(map(lambda _: wait_random(max_delay), range(n))))
+            *list(map(lambda _: wait_random(max_delay), range(n))))
     return sorted(res)
