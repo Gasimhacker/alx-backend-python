e8c485a77f15bc8d501c64343ae937f016f96090
Mohamed Ahmed <ma2286572@gmail.com>
2024-07-13 14:37:11 +0000

    Use a loop instead of explicitly calling the function

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -12,11 +12,6 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """Masure the execution time of 4 coroutines running concurrently"""
     start = time.perf_counter()
-    await asyncio.gather(
-            async_comprehension(),
-            async_comprehension(),
-            async_comprehension(),
-            async_comprehension()
-            )
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
     elapsed = time.perf_counter() - start
     return elapsed
