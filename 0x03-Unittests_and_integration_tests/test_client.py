52325c062ce3650d7c3ce62683e9612794fa9527
Mohamed Ahmed <ma2286572@gmail.com>
2024-07-28 00:17:30 +0000

    Try this tmp file

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,25 +1,39 @@
 #!/usr/bin/env python3
-"""Execute a test multiple times with parameterized"""
+"""
+4. Parameterize and patch as decorators
+5. Mocking a property
+6. More patching
+7. Parameterize
+8. Integration test: fixtures
+9. Integration tests
+"""
 import unittest
-from unittest.mock import Mock, patch
-from client import GithubOrgClient
+from unittest.mock import patch, Mock, PropertyMock
 from parameterized import parameterized
+from client import GithubOrgClient
+from parameterized import parameterized_class
 
 
 class TestGithubOrgClient(unittest.TestCase):
-    """Test the GithubOrgClient.org method"""
+    """
+    A class to test github org client
+    """
 
-    @parameterized.expand([
-        ("google",),
-        ("abc",),
-    ])
-    @patch('client.get_json', return_value={"payload": True})
-    def test_org(self, org_name: str, mock_method: Mock) -> None:
-        """test that org method returns the expected result"""
-        expected = {"payload": True}
-        url = f"https://api.github.com/orgs/{org_name}"
-
+    @parameterized.expand(
+        [
+            ("google"),
+            ("abc"),
+        ]
+    )
+    @patch("client.get_json", return_value={"payload": True})
+    def test_org(self, org_name: str, mock_get_json: Mock) -> None:
+        """
+        Test for org
+        """
+        json_data = {'login': org_name, 'id': 12345}
+        mock_get_json.return_value = json_data
         client = GithubOrgClient(org_name)
-        result = client.org
-        mock_method.assert_called_once_with(url)
-        self.assertEqual(result, expected)
+        response = client.org
+        url = f"https://api.github.com/orgs/{org_name}"
+        mock_get_json.assert_called_once_with(url)
+        self.assertEqual(response, json_data)
